f5ac87ca693c1e980970b7c8831ab7edc962a879
Samiha Ahsan <137447535+SamihaAhsan@users.noreply.github.com>
2025-05-26 23:11:08 -0400

    Update data_input.py

--- Book Classifier/data_input.py
+++ Book Classifier/data_input.py
@@ -3,52 +3,31 @@ import csv #to use functions in csv library
 import http.client #thing that has functions to let you connect to an API and access info
 
 conn = http.client.HTTPSConnection("goodreads-books.p.rapidapi.com") #contains the connection
-conn_two=http.client.HTTPSConnection("goodreads12.p.rapidapi.com")
+
 
 headers_api1 = { 
     'x-rapidapi-key': "bef032f76cmshbabab80030cc0c3p1eb522jsnadf7cbf3450b",
     'x-rapidapi-host': "goodreads-books.p.rapidapi.com"
 }
 
-headers_api2 = {
-    'x-rapidapi-key': "bef032f76cmshbabab80030cc0c3p1eb522jsnadf7cbf3450b",
-    'x-rapidapi-host': "goodreads12.p.rapidapi.com"
-}
+ #contains info of who I am
  #contains info of who I am
 
-def pulling_summary(idd):
-    conn_two.request("GET", "/getBookByID?bookID=56732449", headers=headers_api2)
-    res = conn_two.getresponse()
-    data = res.read()
-    convert=data.decode("utf-8")
-    final_dict=json.loads(convert)
-    return final_dict['description']
+books_arr=["Romance", "Mystery", "Sci-fi", "Western", "Fiction"]
 
-def adding_csv(title, summary, genre):
-    with open('books.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([title, genre, summary])
+def adding_csv(genre):
+    conn.request("GET", "/genres/"+genre+"/best", headers=headers_api1) #asking connection what we want to get
+    res = conn.getresponse() #asking connection to give responses
+    data = res.read() #it's in byte form so take it and convert
+    convert=data.decode("utf-8") #convert even more, to json format
+    final_dict=json.loads(convert) #convert json format to a python dictionary
+    for i in final_dict['books']:
+        title=i['title']
+        with open('books.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([title, genre])
         
+for i in books_arr:
+    adding_csv(i)
 
 
-#romance
-conn.request("GET", "/genres/romance/best", headers=headers_api1) #asking connection what we want to get
-
-res = conn.getresponse() #asking connection to give responses
-data = res.read() #it's in byte form so take it and convert
-
-convert=data.decode("utf-8") #convert even more, to json format
-final_dict=json.loads(convert) #convert json format to a python dictionary
-
-for i in final_dict['books']:
-    summary=pulling_summary(i['id'])
-    title=i['title']
-    genre="Romance"
-    adding_csv(title, genre, summary)
-#mystery
-
-conn.request("GET", "/genres/romance/mystery", headers=headers_api1)
-res = conn.getresponse() 
-data = res.read() 
-convert=data.decode("utf-8") #convert even more, to json format
-final_dict=json.loads(convert) #convert json format to a python dictionary
