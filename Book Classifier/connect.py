a53a8061dc9f50245e11e9e32133d83d0052137b
Samiha Ahsan <137447535+SamihaAhsan@users.noreply.github.com>
2025-08-26 23:06:52 -0400

    Update connect.py

--- Book Classifier/connect.py
+++ Book Classifier/connect.py
@@ -1,32 +1,32 @@
+from flask import Flask, request, render_template 
 import joblib
-from flask import Flask, request  # you forgot to import request
-from flask_cors import CORS
 
 
 app = Flask(__name__)
 
-CORS(app)
+
 model = joblib.load("genre_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
 @app.route("/connect", methods=['GET'])
 def predict_genre():
     summary = request.args.get('summary') #treated as a string
+    if summary:
 
+        summary_converted = vectorizer.transform([summary])  # transform summary text
+        genre_code = model.predict(summary_converted)[0]  # get predicted genre code
 
-    summary_converted = vectorizer.transform([summary])  # transform summary text
-    genre_code = model.predict(summary_converted)[0]  # get predicted genre code
+        if genre_code == 0:
+            return render_template("front.html", msg="Romance")
+        elif genre_code == 1:
+            return render_template("front.html", msg="Mystery")
+        elif genre_code == 2:
+            return render_template("front.html", msg="Sci-Fi")
+        elif genre_code == 3:
+            return render_template("front.html", msg="Western")
+        elif genre_code == 4:
+            return render_template("front.html", msg="Fiction")
 
-    if genre_code == 0:
-        return "Romance"
-    elif genre_code == 1:
-        return "Mystery"
-    elif genre_code == 2:
-        return "Sci-fi"
-    elif genre_code == 3:
-        return "Western"
-    elif genre_code == 4:
-        return "Fiction"
-
+    return render_template("front.html", msg=None)
 if __name__ == '__main__':
     app.run(debug=True)
